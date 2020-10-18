9a286f5f94be17ad7296f2332c01fab3fc07d30a
Shubhankar <das.shubhankar97@gmail.com>
2020-10-19 00:45:00 +0530

    first commit

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Inner,
+  Item,
+  Container,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./styles/jumbotron";
+
+export default function Jumbotron({
+  children,
+  direction = "row",
+  ...otherProps
+}) {
+  return (
+    <Item direction={direction} {...otherProps}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbotron.Container = function JumbotronContainer({ children, ...otherProps }) {
+  return <Container {...otherProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = function JumbotronPane({ children, ...otherProps }) {
+  return <Pane {...otherProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = function JumbotronTitle({ children, ...otherProps }) {
+  return <Title {...otherProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...otherProps }) {
+  return <SubTitle {...otherProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = function JumbotronImage({ ...otherProps }) {
+  return <Image {...otherProps} />;
+};
